d4aec6d5a2b5566b1acfd92d7418c2651d4aa754
zymxxxs <zymxxxs@gmail.com>
2020-02-12 10:23:58 +0800

    feat: 完善 data 转 NSHTTPURLResponse 逻辑

--- YMHTTP/Classes/YMTransferState.m
+++ YMHTTP/Classes/YMTransferState.m
@@ -57,17 +57,22 @@
 - (YMTransferState *)byAppendingHTTPHeaderLineData:(NSData *)data error:(NSError *__autoreleasing _Nullable *)error {
     YMParsedResponseHeader *h = [_parsedResponseHeader byAppendingHeaderLine:data];
     if (!h) {
+        // TODO: Error
         *error = [NSError errorWithDomain:@"YMHTTPParseSingleLineError" code:-1 userInfo:nil];
         return nil;
     }
 
     if (h.type == YMParsedResponseHeaderTypeComplete) {
-        // TODO: lines
-        [h createHTTPURLResponseForURL:_url];
+        NSHTTPURLResponse *response = [h createHTTPURLResponseForURL:_url];
+        if (!response) {
+            // TODO: Error
+            *error = [NSError errorWithDomain:@"YMHTTPParseCompleteHeaderError" code:-1 userInfo:nil];
+            return nil;
+        }
         YMParsedResponseHeader *ph = [[YMParsedResponseHeader alloc] init];
         YMTransferState *ts = [[YMTransferState alloc] initWithURL:_url
                                               parsedResponseHeader:ph
-                                                          response:_response
+                                                          response:response
                                                         bodySource:_requestBodySource
                                                      bodyDataDrain:_bodyDataDrain];
         return ts;
@@ -117,30 +122,68 @@
 }
 
 - (NSHTTPURLResponse *)createHTTPURLResponseForURL:(NSURL *)URL {
-    [self createHTTPMessage];
-    return nil;
-}
-
-- (NSString *)createHTTPMessage {
     NSString *head = [_lines firstObject];
     if (!head) return nil;
 
     if ([_lines count] <= 1) return nil;
     NSArray *tail = [_lines subarrayWithRange:NSMakeRange(1, [_lines count] - 1)];
 
-    [self statusLineByLine:head];
+    NSArray *startline = [self statusLineFromLine:head];
+    if (!startline) return nil;
 
-    return nil;
+    NSDictionary *headerFields = [self createHeaderFieldsFromLines:tail];
+    if (!headerFields) return nil;
+
+    NSString *v = startline[0];
+    NSString *s = startline[1];
+    return [[NSHTTPURLResponse alloc] initWithURL:URL
+                                       statusCode:[s integerValue]
+                                      HTTPVersion:v
+                                     headerFields:headerFields];
 }
 
 /// Split a request line into its 3 parts: HTTP-Version SP Status-Code SP Reason-Phrase CRLF
 /// - SeeAlso: https://tools.ietf.org/html/rfc2616#section-6.1
-
-- (NSArray *)statusLineByLine:(NSString *)line {
+- (NSArray *)statusLineFromLine:(NSString *)line {
     NSArray *a = [line componentsSeparatedByString:@" "];
     if ([a count] != 3) return nil;
 
-    return a;
+    NSString *s = a[1];
+
+    NSInteger status = [s integerValue];
+    if (status >= 100 && status <= 999) {
+        return a;
+    } else {
+        return nil;
+    }
+}
+
+- (NSDictionary *)createHeaderFieldsFromLines:(NSArray *)lines {
+    // not same to swift's source
+    NSMutableDictionary *headerFields = nil;
+    for (NSString *line in lines) {
+        NSRange r = [line rangeOfString:@":"];
+        if (r.location != NSNotFound) {
+            NSString *key = [line substringToIndex:r.location];
+            NSString *value = [line substringFromIndex:r.location + 1];
+
+            NSCharacterSet *set = [NSCharacterSet whitespaceAndNewlineCharacterSet];
+            NSString *trimKey = [key stringByTrimmingCharactersInSet:set];
+            NSString *trimValue = [value stringByTrimmingCharactersInSet:set];
+            if (trimKey && trimValue) {
+                if (!headerFields) headerFields = [NSMutableDictionary dictionary];
+                if (headerFields[trimKey]) {
+                    NSString *v = [NSString stringWithFormat:@"%@ %@", headerFields[trimKey], trimValue];
+                    [headerFields setObject:v forKey:trimKey];
+                } else {
+                    headerFields[trimKey] = trimValue;
+                }
+            }
+        } else {
+            continue;
+        }
+    }
+    return [headerFields copy];
 }
 
 #pragma mark - Private Methods
