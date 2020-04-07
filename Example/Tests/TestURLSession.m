ee9e7cf2cfe28c66b24bfd14e54ac9fa80d876c1
zymxxxs <zymxxxs@gmail.com>
2020-04-07 14:29:37 +0800

    feat: modify testDataTaskWithHttpInputStream

--- Example/Tests/TestURLSession.m
+++ Example/Tests/TestURLSession.m
@@ -113,52 +113,79 @@
 }
 
 - (void)testDataTaskWithHttpInputStream {
-    NSString *urlString = @"http://httpbin.org/post";
-    NSURL *url = [NSURL URLWithString:urlString];
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
-    request.HTTPMethod = @"POST";
-    [request setValue:@"en-us" forHTTPHeaderField:@"Accept-Language"];
-    [request setValue:@"text/xml; charset=utf-8" forHTTPHeaderField:@"Content-Type"];
-    [request setValue:@"chunked" forHTTPHeaderField:@"Transfer-Encoding"];
+    NSArray *httpMethods = @[ @"GET", @"PUT", @"POST", @"DELETE" ];
+    NSString *dataString =
+        @"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras congue laoreet facilisis. Sed porta tristique "
+        @"orci. Fusce ut nisl dignissim, tempor tortor id, molestie neque. Nam non tincidunt mi. Integer ac diam quis "
+        @"leo aliquam congue et non magna. In porta mauris suscipit erat pulvinar, sed fringilla quam ornare. Nulla "
+        @"vulputate et ligula vitae sollicitudin. Nulla vel vehicula risus. Quisque eu urna ullamcorper, tincidunt "
+        @"ante vitae, aliquet sem. Suspendisse nec turpis placerat, porttitor ex vel, tristique orci. Maecenas "
+        @"pretium, augue non elementum imperdiet, diam ex vestibulum tortor, non ultrices ante enim iaculis ex.";
+    NSData *bodyData = [dataString dataUsingEncoding:NSUTF8StringEncoding];
+    for (NSString *method in httpMethods) {
+        NSString *urlString = [NSString stringWithFormat:@"https://httpbin.org/%@", [method lowercaseString]];
+        for (id contentType in @[ @"text/plain; charset=utf-8", [NSNull null] ]) {
+            NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:urlString]];
+            request.HTTPMethod = method;
+            request.HTTPBodyStream = [[NSInputStream alloc] initWithData:bodyData];
+            if ([contentType isKindOfClass:[NSString class]]) {
+                [request setValue:contentType forHTTPHeaderField:@"Content-Type"];
+            }
+            [request setValue:@"en-us" forHTTPHeaderField:@"Accept-Language"];
+            [request setValue:@"chunked" forHTTPHeaderField:@"Transfer-Encoding"];
 
-    NSString *dataString = @"testDataTaskWithHttpInputStream";
-    NSData *data = [dataString dataUsingEncoding:NSUTF8StringEncoding];
-    if (!data) {
-        XCTFail();
-        return;
+            XCTestExpectation *e =
+                [self expectationWithDescription:[NSString stringWithFormat:@"%@ %@ testDataTaskWithHttpInputStream",
+                                                                            method,
+                                                                            urlString]];
+            YMSessionDelegate *delegate = [[YMSessionDelegate alloc] initWithExpectation:e];
+            [delegate runWithRequest:request];
+            [self waitForExpectationsWithTimeout:8.f handler:nil];
+
+            if ([method isEqualToString:@"GET"]) {
+                XCTAssertNotNil(delegate.error);
+                XCTAssertEqual(delegate.error.code, NSURLErrorDataLengthExceedsMaximum);
+                XCTAssertEqualObjects(delegate.error.localizedDescription, @"resource exceeds maximum size");
+                XCTAssertNil(delegate.response);
+                XCTAssertEqual(delegate.callbacks.count, 1);
+                XCTAssertEqualObjects(delegate.callbacks[0],
+                                      NSStringFromSelector(@selector(YMURLSession:task:didCompleteWithError:)));
+                XCTAssertNil(delegate.receivedData);
+            } else {
+                XCTAssertNil(delegate.error);
+                XCTAssertNotNil(delegate.response);
+                XCTAssertEqual(delegate.response.statusCode, 200);
+                XCTAssertEqual(delegate.callbacks.count, 3);
+                NSArray *callbacks = @[
+                    NSStringFromSelector(@selector(YMURLSession:task:didReceiveResponse:completionHandler:)),
+                    NSStringFromSelector(@selector(YMURLSession:task:didReceiveData:)),
+                    NSStringFromSelector(@selector(YMURLSession:task:didCompleteWithError:))
+                ];
+                XCTAssertEqualObjects(delegate.callbacks, callbacks);
+                XCTAssertNotNil(delegate.receivedData);
+
+                NSString *contentLength = [delegate.response.allHeaderFields objectForKey:@"Content-Length"];
+                NSInteger cl = contentLength ? contentLength.integerValue : 0;
+                XCTAssertEqual(delegate.receivedData.length, cl);
+
+                NSDictionary *result = [NSJSONSerialization JSONObjectWithData:delegate.receivedData
+                                                                       options:0
+                                                                         error:nil];
+                NSDictionary *headers = result[@"headers"];
+                if ([contentType isKindOfClass:[NSNull class]]) {
+                    if ([method isEqualToString:@"POST"]) {
+                        XCTAssertEqualObjects(headers[@"Content-Type"], @"application/x-www-form-urlencoded");
+                    } else {
+                        XCTAssertNil(headers[@"Content-Type"]);
+                    }
+
+                } else {
+                    XCTAssertEqualObjects(headers[@"Content-Type"], contentType);
+                }
+            }
+        }
     }
-    request.HTTPBodyStream = [[NSInputStream alloc] initWithData:data];
-    [request.HTTPBodyStream open];
 
-    XCTestExpectation *te =
-        [self expectationWithDescription:@"POST testDataTaskWithHttpInputStream: with HTTP Body as InputStream"];
-
-    YMURLSessionConfiguration *config = [YMURLSessionConfiguration defaultSessionConfiguration];
-    YMURLSession *session = [YMURLSession sessionWithConfiguration:config];
-    YMURLSessionTask *task =
-        [session taskWithRequest:request
-               completionHandler:^(
-                   NSData *_Nullable responseData, NSURLResponse *_Nullable response, NSError *_Nullable error) {
-                   XCTAssertNil(error);
-                   XCTAssertNotNil(response);
-                   XCTAssertNotNil(data);
-                   if (![response isKindOfClass:[NSHTTPURLResponse class]]) {
-                       XCTFail("response is invalid");
-                       return;
-                   }
-
-                   NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
-                   XCTAssertEqual(httpResponse.statusCode, 200, @"HTTP response code is not 200");
-
-                   NSDictionary *value = [NSJSONSerialization JSONObjectWithData:responseData
-                                                                         options:NSJSONReadingMutableContainers
-                                                                           error:nil];
-                   NSString *v = value[@"data"] ?: @"";
-                   XCTAssertEqualObjects(v, dataString, @"Response Data and Data is not equal");
-                   [te fulfill];
-               }];
-    [task resume];
-    [self waitForExpectationsWithTimeout:12 handler:nil];
 }
 
 - (void)testGzippedDataTask {
